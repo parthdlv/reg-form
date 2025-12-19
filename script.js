6b5128586282e8b92fbd8647609910f5c164c874
parthdlv <parthdalvi006@gmail.com>
2025-12-19 23:23:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function jumpscare() {
+  document.body.innerHTML = `
+    <div style="
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: black;
+      z-index: 9999;
+    ">
+      <img src="assets/scary.jpg" style="
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      ">
+      <audio autoplay>
+        <source src="assets/scream.mp3" type="audio/mpeg">
+      </audio>
+    </div>
+  `;
+}
